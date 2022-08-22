6c57f8774ea006b793a3a56315a646b332124258
navdesign <navninderbenipal@gmail.com>
2022-08-22 13:43:26 +0530

    implement stay active

--- src/components/buttons/sideNavButton.jsx
+++ src/components/buttons/sideNavButton.jsx
@@ -1,7 +1,12 @@
 export const SideNavButton = (props) => {
   return (
     <button
-      className="hover:bg-purple-400 px-2 py-2 rounded-md transition-all"
+      className={[
+        props.active ? 'bg-purple-400' : '',
+        'hover:bg-purple-400 px-2 py-2 rounded-md transition-all',
+      ]
+        .filter(Boolean)
+        .join(' ')}
       onClick={props.onClick}
     >
       {props.children}
