104aefb94799171d0bc7be9670c82fec6af0ec3d
navdesign <navninderbenipal@gmail.com>
2022-08-22 14:34:31 +0530

    update read,e

--- src/components/readme.jsx
+++ src/components/readme.jsx
@@ -1,7 +1,7 @@
 const Readme = () => {
   return (
     <>
-      <div className="h-screen bg-white w-full shadow-lg px-5 py-10 space-y-4 text-sm overflow-y-auto inset-y-0">
+      <div className="h-screen bg-white w-full shadow-lg px-8 py-10 space-y-4 text-sm overflow-y-auto inset-y-0">
         <h2 className="text-base font-bold text-cyan-900 border-b">
           A Replica of a Recruitment tool built for managing 1000+ candidate
           applications.
@@ -20,15 +20,14 @@ const Readme = () => {
         </ul>
 
         <p>
-          The icons used in this replica are take from heroicons.com, hence,
-          there is a difference in the appearance.
+          The icons used in this replica are take from{' '}
+          <a href="https://heroicons.com" className="underline font-bold">
+            heroicons.com
+          </a>
+          , hence, I apologize for the slight difference in the appearance.
         </p>
 
-        <p>
-          TailwindCSS framework is used as it helps me write css faster. I
-          believe only someone who is experienced with CSS would be able to use
-          it.
-        </p>
+        <p>TailwindCSS and Vite.js is used for faster developmnent.</p>
         <p>
           No other external library is used except:
           <br />
@@ -52,6 +51,19 @@ const Readme = () => {
           Search feature is added as asked for this assignment. No, external
           APIs are called but the search is performed with the data available in
           memory.
+        </p>
+
+        <div className="flex items-center space-x-4 min-w-max">
+          <p className="font-bold">Lighthouse score:</p>
+          <div className="w-12 h-12 border-4 border-green-600 rounded-full relative">
+            <p className="text-base font-bold absolute top-1/4 ml-1.5 text-green-700">
+              100
+            </p>
+          </div>
+        </div>
+        <p>
+          Lighthouse score of 100 is acheived with this replica. This can be
+          confirmed by navigating to developer tools &gt; Lighthouse.
         </p>
       </div>
     </>
