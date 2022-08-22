892401c926e548e53aa1d8723ecd6d25056f86a8
navdesign <navninderbenipal@gmail.com>
2022-08-22 21:46:50 +0530

    add sr-only class

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,48 @@ import SideBar from './components/sidebar';
 import TopBar from './components/topBar';
 import JobsBoard from './pages/jobsBoard';
 import Readme from './components/readme';
+import { DataProvider } from './components/dataContext/dataContext';
+import { ChevronRightIcon } from './components/icons-dashboard/chevronRightIcon';
+import { useState } from 'react';
 
 export default function App() {
+  const [showReadme, setShowReadme] = useState(false);
+
+  const filterClass = (...classes) => {
+    return classes.filter(Boolean).join(' ');
+  };
+
   return (
-    <div className="flex bg-slate-100">
+    <div className="w-full flex">
       <SideBar>
-        <div className="pl-16 flex flex-col flex-1 w-full min-w-max max-w-7xl">
-          <TopBar />
-          <JobsBoard />
+        <div className="pl-16 flex flex-col flex-1">
+          <DataProvider>
+            <TopBar />
+            <JobsBoard />
+          </DataProvider>
         </div>
       </SideBar>
 
-      <Readme />
+      <div
+        className={filterClass(
+          showReadme ? 'w-96 z-20' : '',
+          'flex items-center fixed right-0 top-1/2'
+        )}
+      >
+        <button
+          onClick={() => setShowReadme(!showReadme)}
+          className="z-20 w-10 h-10 bg-white rounded-full flex justify-center items-center border -ml-10"
+        >
+          <span className="sr-only">Open Readme</span>
+          <ChevronRightIcon
+            className={filterClass(
+              showReadme ? '' : 'tranform rotate-180',
+              'h-5 w-6'
+            )}
+          />
+        </button>
+        {showReadme && <Readme show={setShowReadme} />}
+      </div>
     </div>
   );
 }
