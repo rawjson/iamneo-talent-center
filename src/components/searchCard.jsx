c3978d65c8e0370b345a2c87a939e8fb83ade486
navdesign <navninderbenipal@gmail.com>
2022-08-22 22:37:28 +0530

    search alg updated

--- src/components/searchCard.jsx
+++ src/components/searchCard.jsx
@@ -11,28 +11,23 @@ const SearchCard = () => {
     const searchStr = search.trim();
     const candidates = Object.entries(data['candidates']);
     let found = {};
-    for (let i = 0; i < candidates.length; i++) {
-      let l = 1;
-      let r = candidates[i].length - 1;
 
-      while (l <= r) {
-        const str = candidates[i][l].name.trim().slice(0, 4).toLowerCase();
-        if (searchStr.match(str)) {
-          const id = candidates[i][l].id;
-          const data = candidates[i][l];
-          Object.assign(found, { [id]: data });
-        }
-        l++;
+    for (let i = 0; i < candidates.length; i++) {
+      const str = candidates[i][1].name.trim().slice(0, 4).toLowerCase();
+      if (searchStr.match(str)) {
+        const id = candidates[i][1].id;
+        const cdata = candidates[i][1];
+        Object.assign(found, { [id]: cdata });
       }
     }
-
+    // console.log(found);
     const candidate = Object.entries(found);
 
     if (candidate.length) {
       const columns = Object.entries(data['columns']);
       let newColumns = {};
 
-      for (let i in candidate) {
+      for (let i = 0; i < candidate.length; i++) {
         let l = 0;
         let r = columns.length - 1;
 
@@ -41,19 +36,21 @@ const SearchCard = () => {
           for (let j = 0; j < len; j++) {
             if (columns[l][1].candIds[j] === candidate[i][0]) {
               const id = columns[l][0];
-              const data = columns[l][1];
+              const cdata = columns[l][1];
 
-              for (let x = 0; x < data.candIds.length; x++) {
-                if (data.candIds[x] !== candidate[i][0]) {
-                  delete data.candIds.splice(x, 1);
-                }
+              for (let x = 0; x < cdata.candIds.length; x++) {
+                if (cdata.candIds[x] === candidate[i][0]) {
+                  continue;
+                } else cdata.candIds.splice(x, 1);
               }
-              Object.assign(newColumns, { [id]: data });
+
+              Object.assign(newColumns, { [id]: cdata });
             }
           }
           l++;
         }
       }
+
       const columnOrder = Object.keys(newColumns).map((key) => key);
       const newData = {
         columns: {
@@ -70,8 +67,8 @@ const SearchCard = () => {
   };
 
   const onSearch = (e) => {
-    if (e.target.value.length < 3) {
-      reset();
+    if (e.target.value.length === 0) {
+      window.location.reload();
     }
     setSearch(e.target.value);
   };
