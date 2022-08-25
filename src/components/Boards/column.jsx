5a2f49d220856222c43305fe1dd2ce16f887fc7a
navdesign <navninderbenipal@gmail.com>
2022-08-25 12:39:51 +0530

    extract neseted ternary opr.

--- src/components/Boards/column.jsx
+++ src/components/Boards/column.jsx
@@ -3,23 +3,29 @@ import { Droppable } from 'react-beautiful-dnd';
 
 const Column = (props) => {
   const theme = props.column.theme;
+
+  const filterClasses = (...classes) => classes.filter(Boolean).join(' ');
+
+  const applyBorder = () => {
+    switch (theme) {
+      case 'yellow':
+        return 'border-yellow-500';
+      case 'red':
+        return 'border-red-500';
+      default:
+        return 'border-gray-500';
+    }
+  };
+
   return (
     <>
       <div className="w-80 text-cyan-900">
         {/* column type */}
         <div
-          className={[
-            theme === 'yellow'
-              ? 'border-yellow-500'
-              : theme === 'red'
-              ? 'border-red-500'
-              : theme === 'gray'
-              ? 'border-gray-500'
-              : '',
-            'bg-white h-10 flex items-center rounded border-l-4',
-          ]
-            .filter(Boolean)
-            .join(' ')}
+          className={filterClasses(
+            applyBorder(theme),
+            'bg-white h-10 flex items-center rounded border-l-4'
+          )}
         >
           <h2 className="px-4 space-x-2 text-sm font-bold">
             <span>{props.column.title}</span>
